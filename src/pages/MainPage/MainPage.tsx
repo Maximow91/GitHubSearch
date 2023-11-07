a33acb997fe720b3ee31a34f2d76e738eb17e535
kiryl_maximow <markizzzzka91@gmail.com>
2023-11-07 16:53:34 +0300

    oktocit added

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,26 @@
 import cls from './MainPage.module.scss'
 import {classNames} from '../../lib/classNames/classNames'
+import {Octokit} from '@octokit/core'
+import {useEffect} from 'react'
 
 const MainPage = () => {
+  const octokit = new Octokit({
+    auth: `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`,
+  })
+
+  const getRepos = async () => {
+    const response = await octokit.request('GET /orgs/facebook/repos', {
+      org: 'octokit',
+      type: 'all',
+    })
+
+    console.log(response)
+  }
+
+  useEffect(() => {
+    getRepos()
+  }, [])
+
   return (
     <div className={classNames(cls.MainPage)}>
       <h1>GitHubSearch</h1>
